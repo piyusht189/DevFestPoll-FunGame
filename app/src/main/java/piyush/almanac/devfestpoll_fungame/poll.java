11a548bc651571be81b90a09c60da902cf6b7de0
Super-Nova <piyusht189@gmail.com>
2016-10-10 15:29:17 +0530

    -updated

--- app/src/main/java/piyush/almanac/devfestpoll_fungame/poll.java
+++ app/src/main/java/piyush/almanac/devfestpoll_fungame/poll.java
@@ -8,25 +8,29 @@ import android.view.View;
 import android.widget.Button;
 
 public class poll extends AppCompatActivity {
-
+    Button btn,btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poll);
-
-
+        btn=(Button) findViewById(R.id.good);
+        btn1=(Button) findViewById(R.id.bad);
+        btn.setEnabled(true);
+        btn1.setEnabled(true);
     }
 
     public void good(View view){
         sendvote("good");
-        Button btn=(Button) findViewById(R.id.good);
-        
+
+        btn.setEnabled(false);
         SystemClock.sleep(10000);
         startActivity(new Intent(poll.this,poll.class));
         finish();
     }
     public void bad(View view){
         sendvote("bad");
+
+        btn1.setEnabled(false);
         SystemClock.sleep(10000);
         startActivity(new Intent(poll.this,poll.class));
         finish();
